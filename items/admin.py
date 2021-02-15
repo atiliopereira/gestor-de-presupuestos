04ff89b9630d4c4e65993ed6131ceeec1083f213
hgodoy <hgodoy@dissoi.com>
2021-02-15 11:33:36 -0300

    Corrección de espacios (2)

--- items/admin.py
+++ items/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
-
 from items.models import Rubro, Item, DetalleDeItem
+
 
 class DetalleDeItemInlineAdmin(admin.TabularInline):
     model = DetalleDeItem
     autocomplete_fields = ("material",)
     extra = 0
 
+
 class RubroAdmin(admin.ModelAdmin):
     list_display = ("nombre",)
     ordering = ("nombre",)
     search_fields = ("nombre",)
 
+
 admin.site.register(Rubro, RubroAdmin)
+
 
 class ItemAdmin(admin.ModelAdmin):
     ordering = ("descripcion",)
@@ -21,12 +24,7 @@ class ItemAdmin(admin.ModelAdmin):
     autocomplete_fields = ("rubro",)
     inlines = (DetalleDeItemInlineAdmin,)
 
+
 admin.site.register(Item, ItemAdmin)
 
-#class DetalleDeItemAdmin(admin.ModelAdmin):
-#    list_display = ("material", "coeficiente")
-#    ordering = ("material",)
-#    search_fields = ("material",)
-
-#admin.site.register(DetalleDeItem, DetalleDeItemAdmin)
 
