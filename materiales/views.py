195f213570541a9734e1442f4affc422c915f494
hgodoy <hgodoy@dissoi.com>
2021-02-18 11:25:21 -0300

    Creación de DetailView de material

--- materiales/views.py
+++ materiales/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from materiales.models import Material, PrecioDeMaterial
 
-# Create your views here.
+class MaterialDetailView(DetailView):
+    model = Material
+    template_name = "admin/materiales/material/material_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(MaterialDetailView, self).get_context_data(**kwargs)
+        context["precios"] = PrecioDeMaterial.objects.filter(material=self.object).order_by("-inicio_de_vigencia")
+
+        return context
