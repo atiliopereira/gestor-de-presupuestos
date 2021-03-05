1e004408b0b2c3b86f77dbdfc26a09ba9e0bb1c3
hgodoy <hgodoy@dissoi.com>
2021-03-05 13:58:56 -0300

    Creada la vista de detalle de ítem

--- items/views.py
+++ items/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from items.models import Item, DetalleDeItem
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "admin/items/item/item_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ItemDetailView, self).get_context_data(**kwargs)
+        context["detalles"] = DetalleDeItem.objects.filter(item=self.object)
+        return context
