c2a878bfeeafec7eb18eb08f74a154de43d57a6c
hgodoy <hgodoy@dissoi.com>
2021-03-05 16:46:57 -0300

    Creada la vista de detalle de ítem

--- items/urls.py
+++ items/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from items.ajax import get_item
 from items.autocomplete import ItemAutocomplete
+from items.views import ItemDetailView
+
 
 urlpatterns = [
     url(
@@ -10,5 +12,5 @@ urlpatterns = [
         name='item-autocomplete',
     ),
     url('getitem/$', get_item),
+    url(r'^item_detail/(?P<pk>\d+)/$', ItemDetailView.as_view(), name='item_detail')
 ]
-
