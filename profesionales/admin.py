0d62f32205f3777857589044124ec6fd961d63cb
chilu <atilio@dissoi.com>
2021-03-09 10:40:22 -0300

    TILLNER-24 Crear aplicación de Proveedores

--- profesionales/admin.py
+++ profesionales/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from profesionales.models import Servicio, ServicioProfesional, Profesional
+from profesionales.models import Servicio, ServicioProfesional, Profesional, MaterialProveedor, Proveedor
 
 
 class ServicioAdmin(admin.ModelAdmin):
@@ -21,7 +21,7 @@ class ServicioProfesionalInlineAdmin(admin.TabularInline):
 
 class ProfesionalAdmin(admin.ModelAdmin):
     search_fields = ('nombre', 'ruc', 'direccion', 'telefono', 'email')
-    list_display = ('editar', 'nombre', 'ruc', 'direccion', 'telefono', 'email')
+    list_display = ('editar', 'nombre', 'ruc', 'direccion', 'ciudad', 'telefono', 'email', 'get_servicios')
     inlines = (ServicioProfesionalInlineAdmin, )
     actions = None
 
@@ -35,3 +35,27 @@ class ProfesionalAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Profesional, ProfesionalAdmin)
+
+
+class MaterialProveedorInlineAdmin(admin.TabularInline):
+    model = MaterialProveedor
+    autocomplete_fields = ('material', )
+    extra = 1
+
+
+class ProveedorAdmin(admin.ModelAdmin):
+    search_fields = ('nombre', 'ruc', 'direccion', 'telefono', 'email')
+    list_display = ('editar', 'nombre', 'ruc', 'direccion', 'ciudad', 'telefono', 'email', 'get_materiales')
+    inlines = (MaterialProveedorInlineAdmin, )
+    actions = None
+
+    def editar(self, obj):
+        html = '<a href="/admin/profesionales/proveedor/{}" class="icon-block"><i class="fa fa-edit"></i></a>'.format(obj.pk)
+        return mark_safe(html)
+
+    def ver(self, obj):
+        html = '<a href="/admin/profesionales/proveedor_detail/{}" class="icon-block"><i class="fa fa-eye"></i></a>'.format(obj.pk)
+        return mark_safe(html)
+
+
+admin.site.register(Proveedor, ProveedorAdmin)
