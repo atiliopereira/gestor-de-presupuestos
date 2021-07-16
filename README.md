e9ac413258404d90c5b9823be62b2954d36809ca
chilu <atilio@dissoi.com>
2021-07-16 16:38:27 -0400

    Add README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Gestor de presupuestos
+Plataforma de gestión de presupuestos.
+
+Este es un fork con fines didácticos.
+
+Los presupuestos consisten en items, que cada uno tiene materiales y servicios.
+Estos materiales y servicios tienen un coeficiente de ponderación.
+
+Luego los prespuestos se pueden exportar a excel, pdf u obtener costeos
+de los materiales y servicios por separado.
+
+## Algunas cosas interesantes
+- Crear reportes en excel
+- Crear materiales o servic a partir de una plantilla excel
+- Reportes PDF con reportlab
+- Gran personalización del admin por defecto de django
+
--- tillner/settings.py
+++ tillner/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k@^4a&-y5(*$887kz9y6+8ze+5i4yhbm@3nxo2t&1=%_&-0#@z'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'ec2-18-221-85-216.us-east-2.compute.amazonaws.com',
-                 '18.221.85.216', 'construyaenlinea.dissoi.com']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', ]
 
 
 # Application definition
